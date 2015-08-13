c9d4dbdf8f4c08f310240e326ffa8a852a8b42d6
Jacob MacDonald <jakemac@google.com>
2015-08-13 13:32:23 -0700

    regen with new custom_element_apigen to get @BehaviorProxy annotations, and make the neon_animated_pages_test pass

---
+++ test/fixtures/neon_test_resizable_pages.dart
@@ -0,0 +1,34 @@
+@HtmlImport('neon_test_resizable_pages.html')
+library polymer_elements.test.fixtures.neon_test_resizable_pages;
+
+import 'dart:html';
+import 'package:polymer/polymer.dart';
+import 'package:web_components/web_components.dart' show HtmlImport;
+import 'package:polymer_elements/neon_shared_element_animatable_behavior.dart';
+import 'package:polymer_elements/iron_resizable_behavior.dart';
+// For the html import
+import 'package:polymer_elements/paper_styles.dart';
+
+@jsProxyReflectable
+@PolymerRegister('a-resizable-page')
+class AResizablePage extends PolymerElement
+    with NeonSharedElementAnimatableBehavior, IronResizableBehavior {
+  factory AResizablePage() => new Element.tag('a-resizable-page');
+  AResizablePage.created() : super.created();
+}
+
+@jsProxyReflectable
+@PolymerRegister('b-resizable-page')
+class BResizablePage extends PolymerElement
+    with NeonSharedElementAnimatableBehavior, IronResizableBehavior {
+  factory BResizablePage() => new Element.tag('b-resizable-page');
+  BResizablePage.created() : super.created();
+}
+
+@jsProxyReflectable
+@PolymerRegister('c-resizable-page')
+class CResizablePage extends PolymerElement
+    with NeonSharedElementAnimatableBehavior, IronResizableBehavior {
+  factory CResizablePage() => new Element.tag('c-resizable-page');
+  CResizablePage.created() : super.created();
+}
--- test/neon_animated_pages_test.dart
+++ test/neon_animated_pages_test.dart
@@ -1,29 +1,29 @@
 @TestOn('browser')
 library polymer_elements.test.neon_animated_pages_test;
 
+import 'dart:html';
+import 'package:polymer/polymer.dart';
 import 'package:polymer_interop/polymer_interop.dart';
 import 'package:polymer_elements/neon_animated_pages.dart';
 import 'package:test/test.dart';
 import 'package:web_components/web_components.dart';
 import 'common.dart';
-import 'dart:html';
+import 'fixtures/neon_test_resizable_pages.dart';
 
 main() async {
-
-  await initWebComponents();
+  await initPolymer();
 
   group('<neon-animated-pages>', () {
-
-    group('notify-resize',(){
-
-      test('only a destination page receives a resize event',() async {
+    group('notify-resize', () {
+      test('only a destination page receives a resize event', () async {
         NeonAnimatedPages animatedPages = fixture('notify-resize');
         List<Element> resizables = Polymer.dom(animatedPages).children;
         Map receives = {};
-        resizables.forEach((Element page){
-          page.on['iron-resize'].listen((Event e){
-            var pageName = event.currentTarget.tagName;
-            receives[pageName] = receives.containsKey(pageName) ? receives[pageName]+1 : 1;
+        resizables.forEach((Element page) {
+          page.on['iron-resize'].listen((Event e) {
+            var pageName = (e.currentTarget as Element).tagName;
+            receives[pageName] =
+                receives.containsKey(pageName) ? receives[pageName] + 1 : 1;
           });
         });
 
@@ -31,13 +31,8 @@ main() async {
 
         await wait(50);
 
-        expect(receives,equals({'C-RESIZABLE-PAGE':1}));
-
-      }, skip:'https://github.com/dart-lang/polymer-dart/issues/551');
-
-
-
+        expect(receives, equals({'C-RESIZABLE-PAGE': 1}));
+      });
     });
-
   });
 }
