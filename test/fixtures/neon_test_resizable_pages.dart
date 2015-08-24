0ed947c20c24412f2a3b3d1bde5b647c7fdef9c9
Jacob MacDonald <jakemac@google.com>
2015-08-24 12:56:04 -0700

    added paper-button-behavior test and fixed up some other tests

--- test/fixtures/neon_test_resizable_pages.dart
+++ test/fixtures/neon_test_resizable_pages.dart
@@ -4,6 +4,7 @@ library polymer_elements.test.fixtures.neon_test_resizable_pages;
 import 'dart:html';
 import 'package:polymer/polymer.dart';
 import 'package:web_components/web_components.dart' show HtmlImport;
+import 'package:polymer_elements/neon_animatable_behavior.dart';
 import 'package:polymer_elements/neon_shared_element_animatable_behavior.dart';
 import 'package:polymer_elements/iron_resizable_behavior.dart';
 // For the html import
@@ -12,7 +13,7 @@ import 'package:polymer_elements/paper_styles.dart';
 @jsProxyReflectable
 @PolymerRegister('a-resizable-page')
 class AResizablePage extends PolymerElement
-    with NeonSharedElementAnimatableBehavior, IronResizableBehavior {
+    with NeonAnimatableBehavior, NeonSharedElementAnimatableBehavior, IronResizableBehavior {
   factory AResizablePage() => new Element.tag('a-resizable-page');
   AResizablePage.created() : super.created();
 }
@@ -20,7 +21,7 @@ class AResizablePage extends PolymerElement
 @jsProxyReflectable
 @PolymerRegister('b-resizable-page')
 class BResizablePage extends PolymerElement
-    with NeonSharedElementAnimatableBehavior, IronResizableBehavior {
+    with NeonAnimatableBehavior, NeonSharedElementAnimatableBehavior, IronResizableBehavior {
   factory BResizablePage() => new Element.tag('b-resizable-page');
   BResizablePage.created() : super.created();
 }
@@ -28,7 +29,7 @@ class BResizablePage extends PolymerElement
 @jsProxyReflectable
 @PolymerRegister('c-resizable-page')
 class CResizablePage extends PolymerElement
-    with NeonSharedElementAnimatableBehavior, IronResizableBehavior {
+    with NeonAnimatableBehavior, NeonSharedElementAnimatableBehavior, IronResizableBehavior {
   factory CResizablePage() => new Element.tag('c-resizable-page');
   CResizablePage.created() : super.created();
 }
